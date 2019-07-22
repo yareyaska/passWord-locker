3fb16e718ef69087f7abcb9f0eb3f9674c4bb80c
yareyaska <uraharahussein@gmail.com>
2019-07-22 10:27:08 -0400

    modified run.py

--- exp.py
+++
@@ -1,5 +0,0 @@
-import os, math, string, struct
-
-def generate_password(pass_len):
-    symbols = string.printable.strip()
-    return ''.join([symbols[x * len(symbols) / 256] for x in struct.unpack('%dB' % (pass_len,), os.urandom(pass_len))])
